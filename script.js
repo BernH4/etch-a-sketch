26062b773f2468350345e75c847b60c008ab565d
BernH4 <bernhard.steidl14mail.com>
2020-05-25 20:18:49 +0200

    many Tools added

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ const gridContainer = document.getElementById("grid-container");
 const btnReset = document.getElementById("btn-reset");
 const gridItems = gridContainer.getElementsByTagName('div');
 const inputGridSize = document.getElementById("input-grid-size");
+const tools = document.querySelectorAll('#side-bar *');
 
 generateGrid(16,16);
+
+let currentColor = "hsl(040, 100%, 067%)";
+let mode = "hsl(040, 100%, 067%)";
+tools.forEach((tool) => {
+    tool.addEventListener('click', (e) => {
+        mode = e.target.getAttribute("data-tool");
+        console.log(mode)
+    });
+});
+
 
 btnReset.addEventListener("click", function(e) {
     resetGrid("colors");
@@ -28,11 +39,6 @@ inputGridSize.addEventListener("keydown", function(e) {
     }
 })
 
-
-
-
-
-
 function generateGrid(rows, columns){
     let divcounter = 0;
     gridContainer.style.gridTemplateRows = `repeat${rows}, 1fr)`;
@@ -42,7 +48,9 @@ function generateGrid(rows, columns){
            newDiv = document.createElement("div");
            newDiv.style.gridRow = i
            newDiv.style.gridColumn = j
-           newDiv.addEventListener('mouseover', testFunction);
+           newDiv.style.backgroundColor = "hsl(040, 100%, 100%)";
+           newDiv.setAttribute("color", "hsl(040, 100%, 100%)");
+           newDiv.addEventListener('mouseover', draw);
            gridContainer.appendChild(newDiv);
            divcounter++;
         }
@@ -62,7 +70,73 @@ function resetGrid(operation) {
     });
 } 
 
-function testFunction(e) {
-    e.target.style.backgroundColor = "lightblue";
+function draw(e) {
+    if (mode == "rainbow") {
+        currentColor = getRandomColor();
+    }
+    else if (mode == "lighten") {
+        colorBefore = Array.from(e.target.getAttribute("color"));
+        newColorValue = +(colorBefore[15] + colorBefore[16] + colorBefore[17]) + 10;
+        newColor = Array.from(String(newColorValue));
+        console.log(newColor)
+        if (newColorValue > 100) {
+            colorBefore[15] = 1;
+            colorBefore[16] = 0; 
+            colorBefore[17] = 0;
+        } 
+        else if (newColor.length == 2) { 
+            colorBefore[16] = newColor[0];
+            colorBefore[17] = newColor[1];
+        }
+        else if (newColor.length == 3) {
+            colorBefore[15] = newColor[0];
+            colorBefore[16] = newColor[1];
+            colorBefore[17] = newColor[2];
+        }
+        currentColor = colorBefore.join("");
+    }
+    else if (mode == "darken") {
+        colorBefore = Array.from(e.target.getAttribute("color"));
+        newColorValue = +(colorBefore[15] + colorBefore[16] + colorBefore[17]) - 10;
+        console.log(newColorValue)
+        newColor = Array.from(String(newColorValue));
+        console.log(colorBefore)
+        console.log(newColor)
+
+        if (newColorValue <= 0 || newColor.length == 1) {
+            colorBefore[15] = 0;
+            colorBefore[16] = 0; 
+            colorBefore[17] = 0;
+
+        } 
+        else if (newColor.length == 2) {
+            colorBefore[15] = 0;
+            colorBefore[16] = newColor[0];
+            colorBefore[17] = newColor[1];
+        }
+        else if (newColor.length == 3) {
+            colorBefore[15] = newColor[0];
+            colorBefore[16] = newColor[1];
+            colorBefore[17] = newColor[2];
+        }
+        currentColor = colorBefore.join("");
+    }
+    else {
+        currentColor = mode;
+    }
+    e.target.style.backgroundColor = currentColor;
+    e.target.setAttribute("color", currentColor);
 }
 
+function getRandomColor() {
+    //It wont work with a value under 100 because of the array syntax i use in func lighten and darken
+    let h = randomVal(100,360);
+    let s = randomVal(30, 95);
+    let l = randomVal(30, 80);
+    returnvalue = `hsl(${h}, 0${s}%, 0${l}%)`
+    console.log(returnvalue);
+    return returnvalue;
+}
+function randomVal(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+  }
---
+++ test.js
@@ -0,0 +1,10 @@
+color = "hsl(000, 100%, 050%)"
+console.log(color[13])
+console.log(color[14])
+console.log(color[15])
+console.log(color[16])
+
+wanted = color[16];
+
+newcolor = +(wanted) + 1;
+console.log(typeof(newcolor))
